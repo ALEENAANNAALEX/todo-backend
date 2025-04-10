cda9779bd5d84d71af375ecb83b7a79e78d2a931
Aleena <aleenaannaalex2026@mca.ajce.in>
2025-04-10 11:21:42 +0530

    new commit

--- server.js
+++ server.js
@@ -5,17 +5,14 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Atlas connection
 const MONGODB_URI = `${process.env.MONGODB_URI}`.replace('/?', '/todo-app?');
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB Atlas');
-    // Start the server only after successful database connection
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
@@ -26,11 +23,9 @@ mongoose.connect(MONGODB_URI)
     process.exit(1);
   });
 
-// Basic route for testing
 app.get('/', (req, res) => {
   res.json({ message: 'Todo API is running' });
 });
 
-// Todo routes
 const todoRoutes = require('./routes/todos');
 app.use('/api/todos', todoRoutes); 
